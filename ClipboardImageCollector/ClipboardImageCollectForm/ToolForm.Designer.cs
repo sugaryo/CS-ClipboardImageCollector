86f4e9eda1c556bfa0d50b521fa8ca9076ac9627
ryota.sugawara <ryo.sugawara.1224@gmail.com>
2020-08-05 14:12:28 +0900

    Formプロジェクトで作り直し、取り敢えずクリップボードのリスナ登録、クリップボードのテキストを拾って表示する参考実装。

---
+++ ClipboardImageCollector/ClipboardImageCollectForm/ToolForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ClipboardImageCollectForm
+{
+    partial class ToolForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.txtConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConsole.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtConsole.ForeColor = System.Drawing.Color.Azure;
+            this.txtConsole.Location = new System.Drawing.Point(0, 0);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtConsole.Size = new System.Drawing.Size(800, 450);
+            this.txtConsole.TabIndex = 0;
+            // 
+            // ToolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtConsole);
+            this.Name = "ToolForm";
+            this.Text = "clipboard image collector";
+            this.Load += new System.EventHandler(this.ToolForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConsole;
+    }
+}
+
